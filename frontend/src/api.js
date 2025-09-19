97be135dc70afe302f30120fbe09435719fbc26a
tesla <tesla@teslas-MacBook-Pro.local>
2025-09-20 00:44:36 +0530

    url problem

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,6 @@
 export async function generateSlides(data, token) {
-    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL_LOCAL}/generate_slides`, {
+    const ep = '/generate_slides';
+    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}${ep}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" , 'Authorization': `Bearer ${token}`},
       body: JSON.stringify(data),
