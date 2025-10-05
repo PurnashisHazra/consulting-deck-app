2c03996f0c30282062fec431ddd238c81bd3460e
tesla <tesla@teslas-MacBook-Pro.local>
2025-10-06 00:33:43 +0530

    signup fix and title logo

--- backend/auth.py
+++ backend/auth.py
@@ -1,7 +1,7 @@
 from fastapi import Body
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
-from passlib.hash import sha256_crypt
+from passlib.hash import sha256_crypt, bcrypt
 import jwt as pyjwt
 from datetime import datetime, timedelta
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -41,7 +41,9 @@ class Token(BaseModel):
 def get_password_hash(password):
     return sha256_crypt.hash(password)
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password, hashed_password, use_bcrypt=False):
+    if use_bcrypt:
+        return bcrypt.verify(plain_password, hashed_password)
     return sha256_crypt.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict):
@@ -77,7 +79,8 @@ async def signup(user: UserCreate):
 @router.post("/login", response_model=Token)
 async def login(user: UserLogin):
     db_user = await users_collection.find_one({"email": user.email})
-    if not db_user or not verify_password(user.password, db_user["password"]):
+    use_bcrypt = db_user and db_user.get("old") == "yes"
+    if not db_user or not verify_password(user.password, db_user["password"], use_bcrypt=use_bcrypt):
         raise HTTPException(status_code=400, detail="Invalid credentials")
     access_token = create_access_token(data={"sub": db_user["email"]})
     return {"access_token": access_token, "token_type": "bearer"}
